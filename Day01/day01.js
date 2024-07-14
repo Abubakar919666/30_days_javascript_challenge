0ecc4fa597a6d5fe44a1fb3f7fad01750c88cf16
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-14 23:23:35 +0500

    add readme

---
+++ Day01/day01.js
@@ -0,0 +1,49 @@
+//  Task 01
+
+var num = 24;
+console.log(num); 
+
+// Task 02
+
+let str = "Abubakar";
+console.log(str); 
+
+//  Task 03
+
+const isVerified = true;
+console.log(isVerified);
+
+//  Task 04
+
+let str1 = "Admission are open!";
+console.log(typeof str);
+let  userId = 1111;
+console.log(typeof userId);
+const class_5_Done =  true;
+console.log(typeof class_5_Done);
+let students = []
+console.log(typeof students);
+const carName = {
+    name:"Bughati",
+    model:"500",
+    color:"white"
+};
+console.log(typeof carName);
+
+//  Task 05
+
+let car = "A black Vigo";
+console.log(car);
+car = "A white Vigo";
+console.log(car);
+
+
+//  Task 06 
+
+const Id = 119;
+console.log(Id);
+Id = 120;
+console.log(Id);
+
+// Task 07
+
