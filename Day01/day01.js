141a35165cd57124f95e0a44013c4d83aa959ecf
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-15 00:11:00 +0500

    add Feature task

--- Day01/day01.js
+++ Day01/day01.js
@@ -16,34 +16,78 @@ console.log(isVerified);
 //  Task 04
 
 let str1 = "Admission are open!";
-console.log(typeof str);
+console.log(`str1: ${str1}, type: ${typeof str1}`);
 let  userId = 1111;
-console.log(typeof userId);
+console.log(`userId: ${userId}, type: ${typeof userId}`);
 const class_5_Done =  true;
-console.log(typeof class_5_Done);
+console.log(`class_5_Done: ${class_5_Done}, type: ${typeof class_5_Done}`);
 let students = []
-console.log(typeof students);
+console.log(`students: ${students}, type: ${typeof students}`);
 const carName = {
     name:"Bughati",
     model:"500",
     color:"white"
 };
-console.log(typeof carName);
+console.log(`carName: ${carName}, type: ${typeof carName}`);
 
 //  Task 05
 
 let car = "A black Vigo";
-console.log(car);
+console.log(`car: ${car}, type: ${typeof car}`);
 car = "A white Vigo";
-console.log(car);
+console.log(`car: ${car}, type: ${typeof car}`);
 
 
 //  Task 06 
 
 const Id = 119;
-console.log(Id);
-Id = 120;
-console.log(Id);
+console.log('Id (initial):' , Id);
+// Id = 120;
+// console.log('Id (initial):' , Id); // Uncomment the following line to view the error. TypeError: Assignment to constant variable.
 
-// Task 07
 
+
+
+
+  
+// Feature Request: Variable Types Console Log
+//   Write a script that declares variables of different data types and logs both the value and type of each variable to the console.
+ 
+
+var age = 10;
+let str2 = "Hello";
+const isUpdated = true;
+let stdId;
+let sign = null;
+let stdInfo = { class: "nine" };
+let stdName = [1, 2, 3];
+
+console.log(`age: ${age}, type: ${typeof age}`);
+console.log(`str2: ${str2}, type: ${typeof str2}`);
+console.log(`isUpdated: ${isUpdated}, type: ${typeof isUpdated}`);
+console.log(`stdId: ${stdId}, type: ${typeof stdId}`);
+console.log(`sign: ${sign}, type: ${typeof sign}`);
+console.log(`stdInfo: ${stdInfo}, type: ${typeof stdInfo}`);
+console.log(`stdName: ${stdName}, type: ${typeof stdName}`);
+
+
+
+
+
+  
+//    Feature Request: Reassignment Demo
+//    Create a script that demonstrates the difference in behavior between `let` and `const` when it comes to reassignment.
+ 
+
+
+let userName = "Muhammad";
+console.log('userName (initial):', userName);
+
+userName = "Abubakar";
+console.log('userName (reassigned):', userName);
+
+
+const userName1 = "Ali";
+console.log('userName1:', userName1); 
+// userName1 = "raza";
+// console.log("userName1:" , userName1); // Uncomment the following line to view the error. TypeError: Assignment to constant variable.
