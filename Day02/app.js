91cdd1a846c1ec468fba3483917a0e64e953eabd
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-15 22:56:01 +0500

    task 7

---
+++ Day02/app.js
@@ -0,0 +1,42 @@
+
+let firstNumber = 14;
+let secondNumber = 10;
+
+//  Task 01
+let sum = firstNumber + secondNumber;
+console.log(sum);
+
+// Task 02 
+
+let subtr = firstNumber - secondNumber;
+console.log(subtr);
+
+// Task 03
+
+let multiply = firstNumber * secondNumber;
+console.log(multiply);
+
+// Task 04 
+
+let divide = firstNumber / secondNumber;
+console.log(divide);
+
+// Task 05 
+
+let remainder = firstNumber % secondNumber;
+console.log(remainder);
+
+// Task 06
+
+let addAssigment = 15;
+addAssigment += 3;
+console.log(`My name is Abubakar and i'm ${addAssigment} year old`);
+
+// Task 07
+
+let subAssigment = 15;
+subAssigment -= 3;
+console.log(`I read in grade ${subAssigment}`);
+
+// Task 08
+
