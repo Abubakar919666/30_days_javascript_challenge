18a65c1378fca312f8b1d10d04b167d428a8e287
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-18 15:21:00 +0500

    feature done

--- Day02/app.js
+++ Day02/app.js
@@ -6,8 +6,8 @@ let secondNumber = 10;
 let sum = firstNumber + secondNumber;
 console.log(sum);
 
-// Task 02 
 
+// Task 02 
 let subtr = firstNumber - secondNumber;
 console.log(subtr);
 
@@ -85,12 +85,51 @@ console.log(`The number is: ${result}`);
 
 
 
+// Airthmetic Opreations
 
 let num1 = 24;
 let num2 = 12;
 
+// addition
 console.log(`Addition: ${num1 + num2}`);     
+// substraction
 console.log(`Subtraction: ${num1 - num2}`);    
+// multiplication
 console.log(`Multiplication: ${num1 * num2}`);
+// divide
 console.log(`Division: ${num1 / num2}`);    
+// reminder
 console.log(`Remainder: ${num1 % num2}`);      
+
+
+
+
+
+
+// Comparison all opreators
+let myAge = 24;
+let brotherAge = 20;
+
+// Comparison all opreators
+let biggerSize = (myAge > brotherAge)
+console.log(`My age bigger than brother age = ${biggerSize}`);
+
+biggerSize = (myAge < brotherAge)
+console.log(`My age smaller than brother age = ${biggerSize}`);
+
+
+console.log(`My age bigger or equal to than Brother age = ${myAge >= brotherAge}`);
+
+console.log(`Brother age smaller or equal to than my age = ${brotherAge <= myAge}`);
+
+console.log(`brother age equal to my age= ${brotherAge == myAge}`);
+
+console.log(`brother age not equal my age = ${brotherAge !== myAge}`);
+
+console.log(`brother age equal to my age with dta type = ${brotherAge === myAge}`);
+
+
+// Ternary Opreatot
+(myAge > brotherAge) ? console.log('I am the biger brother!') : console.log('big brother');
+
+(myAge < brotherAge) ? console.log('my small brother') : console.log('small brother!');
