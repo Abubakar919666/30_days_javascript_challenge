8e7c488f3a48e4092f871bdeb0a6313d68a189a1
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-22 20:58:48 +0500

    DAY 04

---
+++ Day03/app.js
@@ -0,0 +1,19 @@
+// // Task 1
+
+// let num1 = -9;
+// if (num1 > 0) {
+//     console.log(`${num1} is positive number `);
+// } else if (num1 < 0){
+//     console.log(`${user1} is negative number `);
+// } else {
+//     console.log(`${user1} is egual to zero`);
+// }
+
+
+// //  Task 02
+
+if (age >= 18) {
+    console.log("You are eligible to given the vote ");
+} else {
+    console.log("You are not eligible to given the vote");
+}
