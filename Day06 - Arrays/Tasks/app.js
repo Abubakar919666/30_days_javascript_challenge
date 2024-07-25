ffc2336fec6c4be2e8204739e08d712812821361
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-25 17:57:13 +0500

    Basic array task done

---
+++ Day06 - Arrays/Tasks/app.js
@@ -0,0 +1,46 @@
+// Task 01
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+
+
+// Task 2
+
+// Access first element of arr
+console.log(arr[0]);
+
+// Access last element of arr
+console.log(arr[4]);
+
+
+
+// Task 03
+
+// arr.push(6);
+arr.push(6)
+console.log(arr);
+
+
+
+// Task 04
+
+// pop method
+arr.pop()
+console.log(arr);
+
+
+
+// Task 05
+
+// shift method
+arr.shift()
+console.log(arr);
+
+
+
+// Task 06
+
+// unShift method
+arr.unshift(1)
+console.log(arr);
